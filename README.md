cdbf53845a10a4da02f94f8d29035f2a61d188b9
IKnewOne <ilyafil2012@gmail.com>
2026-02-01 02:56:11 +0200

    Initial fork

---
+++ README.md
@@ -0,0 +1,5 @@
+# Vintage Story: Tree Tapping Mod
+
+This is the repository for the Tree Tapping mod, written for Vintage Story.
+
+The mod can be found on Vintage Story's ModDB [here](https://mods.vintagestory.at/treetapping)
---
+++ TreeTapRedux/TreeTapRedux.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Vintagestory.API.Common;
+
+namespace TreeTapRedux;
+
+public class TreeTapRedux : ModSystem {
+    public static ILogger Logger { get; private set; }
+    public static ICoreAPI Api { get; private set; }
+
+    public override void StartPre(ICoreAPI api) {
+        base.StartPre(api);
+        Logger = Mod.Logger;
+        Api = api;
+    }
+
+    public override void Start(ICoreAPI api) {
+        base.Start(api);
+        api.RegisterBlockBehaviorClass("AttachRestricted", typeof(BehaviorAttachRestricted));
+    }
+
+
+    public override void Dispose() {
+        Logger = null;
+        Api = null;
+        base.Dispose();
+    }
+}
